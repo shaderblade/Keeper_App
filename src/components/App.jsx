d2906847c2092a81111012298819dc890c3acb91
shaderblade <shubhampandey140602@gmail.com>
2023-10-05 16:37:24 +0530

    Add dynamic note display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+const RenderNotes = notes.map((item) => {
+    return (
+        <Note 
+            key = {item.key}
+            title = {item.title}
+            content = {item.content}
+        />
+    )
+});
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {RenderNotes}
             <Footer />
         </div>
     );
